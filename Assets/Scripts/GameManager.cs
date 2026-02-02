d86b8e08a4f0a665cc750d23a7c3f5671d515ac2
LeBud <c.huyghe@rubika-edu.com>
2026-02-02 17:19:38 +0100

    Started implementing splitscreen

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameManager : MonoBehaviour {
+    
+    HashSet<PlayerInput> players = new HashSet<PlayerInput>();
+    
+    private void Awake() {
+        PlayerInputManager.instance.onPlayerJoined += HandlePlayerJoining;
+    }
+
+    private void HandlePlayerJoining(PlayerInput input) {
+        players.Add(input);
+        SetupCamera();
+    }
+
+    private void SetupCamera() {
+        foreach (var player in players) {
+            switch (players.Count) {
+                case 0:
+                    Debug.LogWarning("No players found");
+                    break;
+                case 1:
+                    Debug.Log($"One player only");
+                    break;
+                case 2:
+                    player.camera.rect = new Rect(player.playerIndex == 0 ? 0f : 0.5f, 0f, 0.5f, 1f);
+                    break;
+                case 3:
+                    player.camera.rect = new Rect(
+                        player.playerIndex == 0 ? 0 : player.playerIndex == 1 ? 0.5f : 0, 
+                        player.playerIndex < 2 ? 0.5f : 0, 
+                        player.playerIndex < 2 ? 0.5f : 1, 
+                        0.5f);
+                    break;
+                case 4:
+                    player.camera.rect = new Rect(player.playerIndex % 2 * 0.5f, player.playerIndex < 2 ? 0.5f : 0f, 0.5f, 0.5f);
+                    break;
+            }
+        }
+    }
+}
