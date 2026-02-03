12db7a2b47fa772ecad8f67c9d07718f7f1a2861
moimoimoi04 <antoine.blgr@gmail.com>
2026-02-03 02:37:08 +0100

    début controller

--- Assets/Scripts/Player/Controller.cs
+++ Assets/Scripts/Player/Controller.cs
@@ -1,5 +1,5 @@
 using System;
-using Unity.VisualScripting;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,35 +7,47 @@ public class Controller : MonoBehaviour
 {
     [Header("General Settings")]
     InputSystem_Actions playerInputActions;
-    [SerializeField]  Rigidbody rb;
-    [SerializeField]  Camera    playerCamera;
-    [SerializeField]  Transform cameraTarget;
+    [SerializeField] TMP_Text currentStateTXT;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] Camera    playerCamera;
+    [SerializeField] Transform cameraTarget;
+    [SerializeField] Transform groundRayPosiiton;
     
-    Transform cameraTransform;
-    Transform playerTransform;
+    
+    
 
     [Header("Movement Settings")] 
     [SerializeField] AnimationCurve movementSpeedCurve;
-    float horizontalInput;
-    float verticalInput;
-    float movementTimer;
+    [SerializeField] AnimationCurve decelerationSpeedCurve;
+
+    
 
     [Header("Camera Settings")]
     [SerializeField] float cameraSpeed;
     [SerializeField] float mouseSensitivity = 2f;
     [SerializeField] float verticalLimit    = 80f;
 
-    float cameraHorizontalInput;
-    float cameraVerticalInput;
-    float yaw;
-    float pitch;
+    Transform cameraTransform;
+    Transform playerTransform;
+    float     stopTimer;
+    float     horizontalInput;
+    float     verticalInput;
+    float     movementTimer;
+    float     cameraHorizontalInput;
+    float     cameraVerticalInput;
+    float     yaw;
+    float     pitch;
     
     
     private Action<InputAction.CallbackContext> onMove;
     private Action<InputAction.CallbackContext> onLook;
-
+    
+    private Action onJump;
+    private Action stopMove;
+    
+    
     [Header("State Machine")]
-    public StateMachine<ControlerState> playerStateMachine = new ();
+    public StateMachine<ControlerState> PlayerStateMachine = new ();
     public enum ControlerState
     {
         Idle,
@@ -43,22 +55,93 @@ public class Controller : MonoBehaviour
         Falling,
         Jumping,
     }
-    
+
+    private void Start()
+    {
+        cameraTransform = playerCamera.transform;
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        CreateState();
+    }
+
     void CreateState()
     {
+        //IDLE
+        PlayerStateMachine.Add(new State<ControlerState>(
+            ControlerState.Idle,
+            _onEnter: IdleEnter,
+            _onFixedUpdate: IdleFixedUpdate,
+            _onLateUpdate: IdleLateUpdate,
+            _onExit: IdleExit
+        ));
         
+        //MOVEMENT
+        PlayerStateMachine.Add(new State<ControlerState>(
+            ControlerState.Moving,
+            _onEnter: MoveEnter,
+            _onUpdate: MoveUpdate,
+            _onFixedUpdate: MoveFixedUpdate,
+            _onLateUpdate: MoveLateUpdate
+        ));
+        
+        PlayerStateMachine.ChangeState(ControlerState.Idle);
     }
 
     #region STATE-MACHINE
 
-    #region IDLE
+    #region FUNCTION CALL
 
-    void IdleUpdate()
+    void Update()
     {
-        
+        PlayerStateMachine?.Update();
+        UpdateStateText();
     }
 
+    void FixedUpdate()
+    {
+        PlayerStateMachine?.FixedUpdate();
+    }
+
+    void LateUpdate()
+    {
+        PlayerStateMachine?.LateUpdate();
+    }
+
+    #endregion
+
+    #region IDLE
+
+    void IdleEnter()
+    {
+        stopTimer = 0;
+        movementTimer = 0;
+    }
+
+    // void IdleUpdate()
+    // {
+    //     
+    // }
+    //
     void IdleFixedUpdate()
+    {
+        stopTimer += Time.fixedDeltaTime;
+        Quaternion targetRotation = Quaternion.Euler(0, yaw, 0);
+        Vector3 forward = targetRotation * Vector3.forward;
+        Vector3 right = targetRotation * Vector3.right;
+    
+        Vector3 move = (forward * verticalInput + right * horizontalInput).normalized;
+        Vector3 velocity = move * decelerationSpeedCurve.Evaluate(stopTimer);
+        velocity.y = rb.linearVelocity.y;
+        
+        rb.linearVelocity = velocity;
+        
+    }
+    
+    void IdleLateUpdate()
+    {
+        CameraMovement();
+    }
+
+    void IdleExit()
     {
         
     }
@@ -67,14 +150,34 @@ public class Controller : MonoBehaviour
     
     #region MOVING
 
-    void MovingUpdate()
+    void MoveEnter()
+    {
+        movementTimer = 0f;
+        stopTimer = 0;
+    }
+    void MoveUpdate()
     {
         
     }
     
-    void MovingFixedUpdate()
+    void MoveFixedUpdate()
     {
+        movementTimer += Time.fixedDeltaTime;
         
+        Quaternion targetRotation = Quaternion.Euler(0, yaw, 0);
+        Vector3 forward = targetRotation * Vector3.forward;
+        Vector3 right = targetRotation * Vector3.right;
+    
+        Vector3 move = (forward * verticalInput + right * horizontalInput).normalized;
+        Vector3 velocity = move * movementSpeedCurve.Evaluate(movementTimer);
+        velocity.y = rb.linearVelocity.y;
+        
+        rb.linearVelocity = velocity;
+    }
+
+    void MoveLateUpdate()
+    {
+        CameraMovement();
     }
 
     #endregion
@@ -108,8 +211,6 @@ public class Controller : MonoBehaviour
     #endregion
 
     #endregion
-    
-    
 
     #region INPUT SYSTEM SETUP
 
@@ -117,6 +218,7 @@ public class Controller : MonoBehaviour
     {
         playerInputActions = new InputSystem_Actions();
         playerInputActions.Enable();
+        AssignActions();
         SubscribeInputSystemActions();
     }
     
@@ -128,14 +230,19 @@ public class Controller : MonoBehaviour
 
     void SubscribeInputSystemActions()
     {
+        //playerInputActions.Player.Jump.started += ctx => onLook?.Invoke(ctx);
+        
         playerInputActions.Player.Move.performed += ctx => onMove?.Invoke(ctx);
         playerInputActions.Player.Look.performed += ctx => onLook?.Invoke(ctx);
+        
+        playerInputActions.Player.Move.canceled += _ => stopMove?.Invoke();
     }
 
     void AssignActions()
     {
         onMove += PlayerMovementInputs;
         onLook += CameraMovementsInputs;
+        stopMove += ResetPlayerMovementInputs;
     }
     
     void UnsubscribeInputSystemActions()
@@ -146,42 +253,30 @@ public class Controller : MonoBehaviour
 
     #endregion
 
-
-    void FixedUpdate()
-    {
-        PlayerMovements();
-    }
-
-    private void LateUpdate()
-    {
-        CameraMovement();
-    }
+    #region INPUT FUNCTIONS
 
     void PlayerMovementInputs(InputAction.CallbackContext context)
     {
+        PlayerStateMachine.ChangeState(ControlerState.Moving);
         horizontalInput = context.ReadValue<Vector2>().x;
         verticalInput = context.ReadValue<Vector2>().y;
     }
-
-    void PlayerMovements()
-    {
-        Quaternion targetRotation = Quaternion.Euler(0, yaw, 0);
-        Vector3 forward = targetRotation * Vector3.forward;
-        Vector3 right = targetRotation * Vector3.right;
     
-        Vector3 move = (forward * verticalInput + right * horizontalInput).normalized;
-        Vector3 velocity = move * movementSpeedCurve.Evaluate(movementTimer);
-        velocity.y = rb.linearVelocity.y;
-        
-        rb.linearVelocity = velocity;
+    void ResetPlayerMovementInputs()
+    {
+        PlayerStateMachine.ChangeState(ControlerState.Idle);
     }
-
-
+    
     void CameraMovementsInputs(InputAction.CallbackContext context)
     {
-        cameraHorizontalInput = context.ReadValue<Vector2>().x;
-        cameraVerticalInput = context.ReadValue<Vector2>().y;
+        cameraHorizontalInput = context.ReadValue<Vector2>().x * mouseSensitivity;
+        cameraVerticalInput = context.ReadValue<Vector2>().y * mouseSensitivity; 
     }
+
+    #endregion
+    
+    
+    #region CAMERA
 
     void CameraMovement()
     {
@@ -189,10 +284,28 @@ public class Controller : MonoBehaviour
         pitch -= cameraVerticalInput;
         pitch = Mathf.Clamp(pitch, -verticalLimit, verticalLimit);
         
-        transform.rotation    = Quaternion.Euler(0, yaw, 0);
-
-        cameraTransform.rotation = Quaternion.Lerp(cameraTransform.rotation, Quaternion.Euler(pitch, yaw, 0), Time.deltaTime * cameraSpeed);
         cameraTransform.position = cameraTarget.position;
+        cameraTransform.rotation = Quaternion.Lerp(cameraTransform.rotation, Quaternion.Euler(pitch, yaw, 0), Time.deltaTime * cameraSpeed);
+        
     }
+
+    #endregion
+
+    #region GROUND
+
+    bool IsGrounded()
+    {
+        Ray ray = new Ray(groundRayPosiiton.position, -groundRayPosiiton.up);
+        return Physics.Raycast(ray, out RaycastHit hit, 1f);
+    }
+
+    #endregion
+
+    void UpdateStateText()
+    {
+        currentStateTXT.text = PlayerStateMachine.currentState.iD.ToString();
+    }
+    
+    //bool isGrounded() => Physics.Raycast(cameraTransform.position, Vector3.down, verticalLimit);
     
 }
