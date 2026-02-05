06620c73c6b6494eaab6e87269ba037dbf53e516
Antoine <a.boulanger@rubika-edu.com>
2026-02-05 16:57:17 +0100

    fix

--- Assets/Scripts/GadgetSystem/Gadgets Var/StickGadget.cs
+++ Assets/Scripts/GadgetSystem/Gadgets Var/StickGadget.cs
@@ -1,23 +1,48 @@
 using System.Collections;
+using Player;
 using UnityEngine;
 
 public class StickGadget : Gadget
 {
     [SerializeField] Rigidbody rb;
+    [Header("Basic Stick")]
     [SerializeField] float     setback;
     [SerializeField] float     range;
     [SerializeField] float     movementTime;
     [SerializeField] float     cooldown;
-
-    private bool canUse = true;
+    
+    [Header("Heavy Stick")]
+    [SerializeField] bool heavyStick;
+    [SerializeField] float maxLoadTime;
+    [SerializeField] float maxLoadForce;
+    
+    private int        currentLoadTime;
+    private Controller player;
+    private bool       canUse = true;
     
     //COOLDOWN
     
     protected override void OnUse()
     {
-        if (!canUse) return;
-        StartCoroutine(Cooldown());
-        StartCoroutine(AnimateGadget());
+        // j'attend que les GD choisissent pour en garder qu'un
+        switch (heavyStick)
+        {
+            case true:
+                if (!canUse) return;
+                StartCoroutine(LoadForce());
+                break;
+            case false:
+                if (!canUse) return;
+                StartCoroutine(Cooldown());
+                StartCoroutine(AnimateGadget());
+                Hit();
+                break;
+        }
+        
+    }
+
+    private void Hit()
+    {
         Ray baseCast = new Ray(GadgetController.concernedPlayerCamera.transform.position, GadgetController.concernedPlayerCamera.transform.forward);
         RaycastHit[] target = Physics.SphereCastAll(baseCast, 0.25f, range);
         foreach (var hit in target)
@@ -41,6 +66,22 @@ public class StickGadget : Gadget
         transform.localRotation = Quaternion.Euler(-90, 0, 0);
         yield return new WaitForSeconds(0.15f);
         transform.localRotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    IEnumerator LoadForce()
+    {
+        canUse =  false;
+        float baseForce = setback;
+        float elapsed   = 0;
+        while (elapsed < maxLoadTime || !player.GetInputs().use.IsPressed())
+        {
+            setback =  Mathf.Lerp(baseForce, maxLoadForce, elapsed / maxLoadTime);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        Hit();
+        setback = baseForce;
+        canUse = true;
     }
 
     public override void OnPickup()
