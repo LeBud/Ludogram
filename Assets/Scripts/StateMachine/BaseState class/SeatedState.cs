d781c7ab85bca8e8aa13bf9aebbd997404418fbe
LeBud <c.huyghe@rubika-edu.com>
2026-02-06 11:50:23 +0100

    Update sitting in car

--- Assets/Scripts/StateMachine/BaseState class/SeatedState.cs
+++ Assets/Scripts/StateMachine/BaseState class/SeatedState.cs
@@ -1,13 +1,12 @@
+using CarScripts;
 using Player;
 using StateMachine.Finite_State_Machine_class;
+using UnityEngine;
 
 namespace StateMachine.BaseState_class
 {
-    public class SeatedState : BaseState
-    {
-        public SeatedState(Controller player) : base(player)
-        {
-        }
+    public class SeatedState : BaseState {
+        public SeatedState(Controller player) : base(player) { }
 
         public override void OnEnter()
         {
@@ -17,6 +16,12 @@ namespace StateMachine.BaseState_class
             
             player.GetInputs().DisablePlayerInput();
             player.GetInputs().EnableCarInput();
+            
+            player.transform.parent = player.currentCar.transform;
+            player.DisableCollider();
+            player.GetRB().isKinematic = true;
+            player.GetRB().interpolation = RigidbodyInterpolation.None;
+            player.transform.position = player.seat.GetPlayerPos().position;
         }
         public override void OnExit()
         {
@@ -26,6 +31,13 @@ namespace StateMachine.BaseState_class
             
             player.GetInputs().DisableCarInput();
             player.GetInputs().EnablePlayerInput();
+            
+            player.transform.parent = null;
+            player.GetRB().isKinematic = false;
+            player.GetRB().interpolation = RigidbodyInterpolation.Interpolate;
+            player.EnableCollider();
+
+            player.seat.UnSeatDriver();
         }
     }
 }
