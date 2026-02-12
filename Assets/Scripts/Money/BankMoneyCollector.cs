84e24a43821a5a93eab6089b9297a3b3593c59a6
Antoine <a.boulanger@rubika-edu.com>
2026-02-12 20:37:50 +0100

    money loop + shop

---
+++ Assets/Scripts/Money/BankMoneyCollector.cs
@@ -0,0 +1,22 @@
+using Manager;
+using UnityEngine;
+
+public class BankMoneyCollector : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out MoneyBag moneyBag))
+        {
+            GameManager.instance.moneyManager.moneySaved += moneyBag.moneyValue;
+            GameManager.instance.moneyManager.DeregisterMoneyBag(moneyBag);
+            Destroy(moneyBag.gameObject);
+        }
+    }
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, transform.localScale);
+    }
+}
